bfd0a6a0a6cf38a02c68dee45013f70ba4ccbc78
nick_local <mykolakolotilo@gmail.com>
2023-10-22 22:51:25 -0600

    remove test files

--- tests-redis/1.js
+++
@@ -1,9 +0,0 @@
-const redis = require('redis');
-
-const redisUrl = 'redis://127.0.0.1:6379'
-
-const client = redis.createClient(redisUrl);
-
-client.set('hey', 'there');
-
-client.get('hey', (err, value) => console.log("redis value: ", value));
